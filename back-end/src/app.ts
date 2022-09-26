a0c7d27b03ee231d00ce2f06fba9d81794d7349c
Mary Karakida <makarakida@gmail.com>
2022-09-26 13:15:13 -0300

    fix: call test router

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,14 +1,23 @@
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 import 'express-async-errors';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
 import recommendationRouter from './routers/recommendationRouter.js';
+import testRouter from './routers/e2eRouter';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/recommendations', recommendationRouter);
+
+if (process.env.NODE_ENV === 'test') {
+    app.use(testRouter);
+}
+
 app.use(errorHandlerMiddleware);
 
 export default app;
